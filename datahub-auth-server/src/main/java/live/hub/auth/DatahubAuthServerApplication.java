b334651055635aacb43147450f1664c8464f814e
阳光很暖 <2063114731@qq.com>
2023-10-02 14:48:28 +0800

    init

---
+++ datahub-auth-server/src/main/java/live/hub/auth/DatahubAuthServerApplication.java
@@ -0,0 +1,13 @@
+package live.hub.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatahubAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatahubAuthServerApplication.class, args);
+    }
+
+}
---
+++ datahub-auth-server/src/test/java/live/hub/auth/DatahubAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package live.hub.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatahubAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
